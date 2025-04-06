2e0894ca1904f84f7f58b878803598c74c82d92b
MontyPan <monty.pan@dontcareabout.us>
2025-04-06 21:48:37 +0800

    增加：StartDateComparator

--- src/main/java/us/dontcareabout/historyChart/client/vo/HasPeriod.java
+++ src/main/java/us/dontcareabout/historyChart/client/vo/HasPeriod.java
@@ -1,8 +1,16 @@
 package us.dontcareabout.historyChart.client.vo;
 
+import java.util.Comparator;
 import java.util.Date;
 
 public interface HasPeriod {
 	Date getStartDate();
 	Date getEndDate();
+
+	public class StartDateComparator<T extends HasPeriod> implements Comparator<T> {
+		@Override
+		public int compare(T o1, T o2) {
+			return o1.getStartDate().compareTo(o2.getStartDate());
+		}
+	}
 }
