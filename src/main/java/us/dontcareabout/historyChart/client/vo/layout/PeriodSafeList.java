7180f5f333b6a3de04f72749ebc344062ce15453
MontyPan <monty.pan@dontcareabout.us>
2025-04-12 11:51:46 +0800

    增加：PeriodSafeList

---
+++ src/main/java/us/dontcareabout/historyChart/client/vo/layout/PeriodSafeList.java
@@ -0,0 +1,54 @@
+package us.dontcareabout.historyChart.client.vo.layout;
+
+import java.util.Date;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import us.dontcareabout.historyChart.client.vo.HasPeriod;
+
+/**
+ * 一組安全、不會重疊的 {@link HasPeriod}。
+ * 亦即：任意兩個 element 的起訖日期區間都不會重疊（但允許相等）。
+ */
+public class PeriodSafeList<T extends HasPeriod> implements HasPeriod {
+	private List<T> list = Lists.newArrayList();
+	private Date startDate;
+	private Date endDate;
+
+	/**
+	 * 前提假設：傳入 period 的開始時間小於 list 中任一元素的開始時間。
+	 */
+	public boolean accept(T newPeriod) {
+		if (list.isEmpty()) {
+			list.add(newPeriod);
+			startDate = newPeriod.getStartDate();
+			endDate = newPeriod.getEndDate();
+			return true;
+		}
+
+		//只要開始時間比目前的結束時間晚就行
+		//如果日期相等也可以接受，所以用 not before
+		if (!newPeriod.getStartDate().before(endDate)) {
+			list.add(newPeriod);
+			endDate = newPeriod.getEndDate();
+			return true;
+		}
+
+		return false;
+	}
+
+	public List<T> getList() {
+		return list;
+	}
+
+	@Override
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	@Override
+	public Date getEndDate() {
+		return endDate;
+	}
+}
