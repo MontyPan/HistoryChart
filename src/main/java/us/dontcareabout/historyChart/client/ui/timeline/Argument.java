7349108e308a845bb7c576e1c3b8592abdb5b05d
MontyPan <monty.pan@dontcareabout.us>
2025-10-25 21:53:17 +0800

    增加：timeline 的參數集中地

---
+++ src/main/java/us/dontcareabout/historyChart/client/ui/timeline/Argument.java
@@ -0,0 +1,11 @@
+package us.dontcareabout.historyChart.client.ui.timeline;
+
+public class Argument {
+	public static double dayUnit = 1.0 / 365;
+
+	public static int padding = 5;
+
+	/** {@link EraLS} 名稱的高度 */
+	public static int eraNameHeight = 26;
+	public static int incidentHeight = 32;
+}
