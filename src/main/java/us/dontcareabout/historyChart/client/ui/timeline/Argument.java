845edac6467f7245c87d268e7b7ef8f2804f4a7d
MontyPan <monty.pan@dontcareabout.us>
2025-11-29 23:47:28 +0800

    增加：toWidth()

--- src/main/java/us/dontcareabout/historyChart/client/ui/timeline/Argument.java
+++ src/main/java/us/dontcareabout/historyChart/client/ui/timeline/Argument.java
@@ -1,11 +1,19 @@
 package us.dontcareabout.historyChart.client.ui.timeline;
 
+import java.util.Date;
+
+import us.dontcareabout.historyChart.client.util.DateUtil;
+
 public class Argument {
-	public static double dayUnit = 1.0 / 365;
+	public static double dayUnit = 1.0 / 200;
 
 	public static int padding = 5;
 
 	/** {@link EraLS} 名稱的高度 */
 	public static int eraNameHeight = 26;
 	public static int incidentHeight = 32;
+
+	public static double toWidth(Date start, Date target) {
+		return DateUtil.daysBetween(start, target) * dayUnit;
+	}
 }
