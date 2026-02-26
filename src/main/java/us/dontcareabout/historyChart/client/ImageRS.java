2b0b735565b5bc7e3af0eeb7d9b7ec973fc8f684
MontyPan <monty.pan@dontcareabout.us>
2026-02-26 22:22:28 +0800

    增加：ImageRS、以及用到的圖檔

---
+++ src/main/java/us/dontcareabout/historyChart/client/ImageRS.java
@@ -0,0 +1,13 @@
+package us.dontcareabout.historyChart.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface ImageRS extends ClientBundle {
+	static ImageRS I = GWT.create(ImageRS.class);
+
+	ImageResource boxArrowUpRight();
+	ImageResource smartHistory();
+	ImageResource wikipedia();
+}
